2368b56ac28d4aa18e8da46feded7baf5f9defb5
Znova Stanislav <93055116+ZnSt@users.noreply.github.com>
2023-05-01 13:24:16 +0300

    add

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-const DB_HOST =
-  'mongodb+srv://libertydDomain:yLW3absosYdR7D4p@cluster0.ocqvs9u.mongodb.net/online_shop?retryWrites=true&w=majority';
+const { DB_HOST } = require('./config');
+
 mongoose
   .connect(DB_HOST)
   .then(() => console.log('DATA connect'))
